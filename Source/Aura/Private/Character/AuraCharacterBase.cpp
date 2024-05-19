aa410893a57c950e097d523d1e59812eea61cffe
kumarbishnu <kumarbishnu202@gmail.com>
2024-05-20 00:25:04 +0545

    feat: Character and Enemy Classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright: Bishnu Kumar, 2024
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+// Copyright: Bishnu Kumar, 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
