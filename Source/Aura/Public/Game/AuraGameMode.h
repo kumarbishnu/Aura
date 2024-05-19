4fd7c5222214a7b6d39b1f32e7df2d34859c2077
kumarbishnu <kumarbishnu202@gmail.com>
2024-05-20 02:28:14 +0545

    feat: Add GameMode, wrap up Character Movement

---
+++ Source/Aura/Public/Game/AuraGameMode.h
@@ -0,0 +1,17 @@
+// Copyright: Bishnu Kumar, 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
